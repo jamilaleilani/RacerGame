ae2087bd79ab44b3e821639a137e2808714e39ed
Jamila Keba <jamilaleilanikeba@gmail.com>
2018-02-02 11:07:58 -0800

    created racer game

---
+++ app.js
@@ -0,0 +1,25 @@
+var purpleCar = document.getElementById("purpleCar");
+var orangeCar = document.getElementById("orangeCar");
+
+function carMoving(letterVal, car, color) {
+  car.style.marginLeft = "0%";
+  document.addEventListener('keyup', function(whichKey) {
+    var carMarginLeft = parseInt(car.style.marginLeft, 10);
+    if (whichKey.keyCode === letterVal && carMarginLeft < 95) {
+      car.style.marginLeft = carMarginLeft + 1 + "%";
+    } else if (carMarginLeft >= 95 && document.querySelector("h1").innerHTML == "Racer Game") {
+      document.querySelector("h1").innerHTML = color +" Wins!";
+    }
+  })
+}
+
+carMoving(76, orangeCar, "Blue");
+carMoving(65, purpleCar, "Red");
+
+
+document.querySelector('h2').addEventListener('click', function() {
+  console.log("reset")
+  blueCar.style.marginLeft = "0%";
+  redCar.style.marginLeft = "0%";
+  document.querySelector("h1").innerHTML = "Racer Game";
+})
